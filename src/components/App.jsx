d29f172b0180dde0d58ce2ede0370c4676cfd6d9
JASMINE <JAZNIEC17@LIVE.COM>
2026-05-16 07:59:50 -0500

    Add useLocation Hook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
-import { useState } from "react"; // New import
-import { Routes, Route, Navigate, useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react"; // New import
+import { Routes, Route, Navigate, useNavigate, useLocation } from "react-router-dom";
 import Ducks from "./Ducks";
 import Login from "./Login";
 import MyProfile from "./MyProfile";
@@ -7,12 +7,36 @@ import Register from "./Register";
 import ProtectedRoute from "./ProtectedRoute";
 import "./styles/App.css";
 import * as auth from '../utils/auth';
+import { setToken, getToken } from "../utils/token";
+import * as api from "../utils/api";
 
 function App() {
   const [userData, setUserData] = useState({ username: "", email: "" });
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   const navigate = useNavigate();
+
+  // Invoke the hook. It's necessary to invoke the hook in both 
+  // components.
+  const location = useLocation();
+
+  useEffect(() => {
+  const jwt = getToken();
+    
+  if (!jwt) {
+    return;
+  }
+
+  api
+      .getUserInfo(jwt)
+      .then(({ username, email }) => {
+        // If the response is successful, log the user in, save their 
+        // data to state, and navigate them to /ducks.
+        setIsLoggedIn(true);
+        setUserData({ username, email });
+      })
+      .catch(console.error);
+}, []);
 
   const handleLogin = ({ username, password }) => {
     if (!username || !password) {
@@ -24,9 +48,16 @@ function App() {
       .then((data) => {
         // Verify that a jwt is included before logging the user in.
         if (data.jwt) {
+          setToken(data.jwt);
           setUserData(data.user);  // save user's data to state
           setIsLoggedIn(true);    // log the user in
-          navigate("/ducks");    // send them to /ducks
+          
+          // After login, instead of navigating always to /ducks, 
+          // navigate to the location that is stored in state. If
+          // there is no stored location, we default to 
+          // redirecting to /ducks.
+          const redirectPath = location.state?.from?.pathname || "/ducks";
+          navigate(redirectPath);
         }
       })
       .catch(console.error);
@@ -39,8 +70,9 @@ function App() {
     confirmPassword,
   }) => {
     if (password === confirmPassword) {
-      auth.register(username, password, email)
-       .then(() => {
+      auth
+        .register(username, password, email)
+        .then(() => {
           navigate("/login");
         })
         .catch(console.error);
@@ -68,17 +100,21 @@ function App() {
       <Route
         path="/login"
         element={
+          <ProtectedRoute isLoggedIn={isLoggedIn} anonymous>
           <div className="loginContainer">
             <Login handleLogin={handleLogin} />
           </div>
+          </ProtectedRoute>
         }
       />
       <Route
         path="/register"
         element={
+          <ProtectedRoute isLoggedIn={isLoggedIn} anonymous>
           <div className="registerContainer">
             <Register handleRegistration={handleRegistration} />
           </div>
+          </ProtectedRoute>
         }
       />
       <Route
